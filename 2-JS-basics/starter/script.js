a4511edf7eb6333605ccb802137a5d66e6a7c35e
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-27 14:40:20 -0400

    operator precedence

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -52,6 +52,7 @@ var johnMark = 'John and Mark';
 /**
   * Basic operators
   */
+ /*
   var year, yearJohn, yearMark;
   now = 2020;
   ageJohn = 28;
@@ -77,3 +78,40 @@ console.log(typeof ageJohn);
 console.log(typeof ' Mark is older than john');
 var x;
 console.log(typeof x);
+*/
+
+/**
+ * Operator precedence
+ */
+/*
+ var now = 2018;
+ var yearJohn = 1989;
+ var fullAge = 18;
+
+
+// Multiple operators
+ var isFullAge = now - yearJohn >= fullAge; //true
+ console.log(isFullAge);
+
+
+// Grouping
+ var ageJohn = now - yearJohn;
+ var ageMark = 35;
+ var average = (ageJohn + ageMark) / 2;
+ console.log(average);
+
+ // Multiple assignments
+ var x, y;
+ x = y = (3 + 5) * 4 - 6; 
+ console.log(x);
+ console.log(y);
+ console.log(x, y);
+
+ // More operators
+//  x = x * 2;
+ x *= 2;
+ console.log(x);
+x += 10;
+console.log(x);
+*/
+
