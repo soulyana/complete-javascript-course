44eb658558cd3afd430e24fbf8a9445a2d959aa4
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-27 14:58:59 -0400

    if / else statements

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -173,3 +173,22 @@ console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
 } else {
     console.log(firstName + ' your lover is on the way...');
 }
+
+var massMark = 78; // kg
+var heightMark = 1.69; // meters
+
+var massJohn = 92;
+var heightJohn = 1.95;
+
+var BMIMark = massMark / (heightMark * heightMark);
+var BMIJohn = massJohn / (heightJohn * heightJohn);
+
+var markHigherBMI = BMIMark > BMIJohn;
+
+// console.log('Is Mark\'s BMI higher than John\'s? ' + markHigherBMI);
+
+if (BMIMark > BMIJohn) {
+    console.log('Mark\'s BMI is higher than John\'s');
+} else {
+    console.log('John\'sBMI is higher than Mark\'s');
+}
