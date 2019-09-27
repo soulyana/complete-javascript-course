f3c1ab1f8daed4bf70041683147c6cb2f4177dd1
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-27 19:05:09 -0400

    coding challenge 3: instructor solution

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -433,6 +433,7 @@ console.log(soulRetirement, mikeRetirement);
 /**
  * Coding Challenge 3
  */
+/*
 
 var tips = [];
 var bills = [];
@@ -490,3 +491,32 @@ var fiftenTip = tipCalculator(148);
 var tenTip = tipCalculator(250);
 console.log(tips);
 console.log(bills);
+*/
+
+/**
+ * Coding Challenge 3 Insturctor solution
+ */
+
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill <= 200) {
+        percentage = .15;
+    } else {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+console.log(tipCalculator(10));
+
+var bills = [124, 48, 268];
+var tips = [tipCalculator(bills[0]),
+tipCalculator(bills[1]),
+tipCalculator(bills[2])];
+console.log(tips);
+var finalBill = [tips[0] + bills[0],
+                tips[1] + bills[1],
+                tips[2] + bills[2]];
+console.log(tips, finalBill);
