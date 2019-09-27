e3c2239a9d7ffab494fe049cafcc463c89d00c53
Soulyana <soulyana.lakew@accenturefederal.com>
2019-09-27 12:08:06 -0400

    variables and data types

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,18 @@
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+var age = 23;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var johnMark = 'John and Mark';
+// reserved keywords can't use in variables
